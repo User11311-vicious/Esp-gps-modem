20da201df42db9f4b35dd1bc8b745846205f4437
User11311-vicious <matveytolkunov373@gmail.com>
2022-10-24 12:05:18 +0300

    Update esp.py

--- esp.py
+++ esp.py
@@ -5,30 +5,28 @@ import socket
 
 indi = shared_memory.ShareableList([0], name="indicate")
 
-def bl():
-    addr = "24:62:AB:E1:96:7A"
-    channel = 1
-    s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-    s.connect((addr, channel))
-    s.settimeout(0.1)
+addr = "24:62:AB:E1:96:7A"
+channel = 1
+s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+s.connect((addr, channel))
 
-    while True:
-        try:
-            if indi[0] == 1:
-                addr = "24:62:AB:E1:96:7A"
-                channel = 1
-                s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-                s.connect((addr, channel))
-                indi[0] = 0
-            data = s.recv(1024)
-            print(indi[0])
-            print ("received [%s]" % data)
-            sleep(1)
-        except:
-            indi[0] = 1
-            print('boulll')
-            sleep(1)
+while True:
+    try:
+        if indi[0] == 1:
+            addr = "24:62:AB:E1:96:7A"
+            channel = 1
+            s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+            s.connect((addr, channel))
+            indi[0] = 0
+        data = s.recv(1024)
+        data = data.decode('utf-8')
+        data = data.split('|')
+        print ("received [%s]" % data)
+        sleep(1)
+    except:
+        indi[0] = 1
+        print('boulll')
+        sleep(1)
 
-bl()
 indi.shm.close()
 indi.shm.unlink()
